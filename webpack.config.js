045d7bd9ebbd7659bb918e06d3174a5637c55147
Granton Mchana <gmchana254@gmail.com>
2019-03-16 19:15:10 +0300

    adding a public path

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,16 @@ const {
 module.exports = {
 
   entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     resolve(__dirname, "src") + "/index.jsx"
   ],
 
   output: {
     filename: 'app.bundle.js',
     path: resolve(__dirname, 'build'),
+    publicPath: '/'
   },
 
   resolve: {
